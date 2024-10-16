41d42880825fb7b4a3a18068cf81fc631f68c38e
siqjulia <jusiqueira@davidson.edu>
2024-10-16 16:39:54 -0400

    Created "songs" array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,129 @@
 
 /* javascript */
+
+const songs = [
+    {
+        title: "I'm Coming Out",
+        artist: "Diana Ross",
+        year: "1980",
+        video: "https://www.youtube.com/embed/F-mjl63e0ms?si=mRzmPnMhQtF_89W1",
+    },
+
+    {
+        title: "I Want to Break Free",
+        artist: "Queen",
+        year: "1984",
+        video: "https://www.youtube.com/embed/f4Mc-NYPHaQ?si=3SggS9XyTQxK9qmH",
+    },
+
+    {
+        title: "American Teenager",
+        artist: "Ethel Cain",
+        year: "2022",
+        video: "https://www.youtube.com/embed/9Ed_WWmFy9I?si=EBwFsandCjSjkNba",
+    },
+
+    {
+        title: "I Know A Place",
+        artist: "MUNA",
+        year: "2017",
+        video: "https://www.youtube.com/embed/-t5gGm3NWU4?si=bgiwDSvWxLagyGsT",
+    },
+
+    {
+        title: "YMCA",
+        artist: "Village People",
+        year: "1978",
+        video: "https://www.youtube.com/embed/9Ed_WWmFy9I?si=k6X_H62chFMmv1M5",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+    {
+        title: "",
+        artist: "",
+        year: "",
+        video: "",
+    },
+
+]
+
+document.addEventListener("click"); {
+    function randomArrayIndex(songs) {
+        return result[Math.floor(Math.random() * arr.length)];
+        console.log(result);
+
+    }
+}
+
+let result =
+
+
