3b6fa23060c2fb6361c3ed07e0fe943988531425
siqjulia <jusiqueira@davidson.edu>
2024-10-27 21:35:25 -0400

    Added Youtube videos and descriptions

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,14 @@
 /* javascript */
 
 const songs = [
+
+/* The descriptions were not written by me. They come from Rolling Stone: https://www.rollingstone.com/music/music-lists/25-essential-lgbtq-pride-songs-199348/mary-lambert-she-keeps-me-warm-2013-112091/ and Billboard Music: https://www.billboard.com/lists/lgbtq-anthems-queer-pride-songs/y-m-c-a-village-people-1978/*/
     {
         title: "I'm Coming Out",
         artist: "Diana Ross,",
         year: "1980",
-        video: "https://www.youtube.com/embed/F-mjl63e0ms?si=mRzmPnMhQtF_89W1", 
+        video: "https://www.youtube.com/embed/maJgjTp0NQ0?si=XdxsyJkFtBS_ShLn",
+        imp: "Even at its conception, this song was a gay anthem: After seeing three drag queens impersonate Ross at a New York discotheque, Nile Rodgers and Bernard Edwards were inspired to write something for her gay fandom. Ross almost got cold feet releasing it but Rodgers convinced her to go with it. She took his advice and landed her sixth Hot 100 top five hit on the Hot 100 as a solo artist. (The song reached No. 5 in November 1980.)",
     },
 
     {
@@ -14,6 +17,7 @@ const songs = [
         artist: "Queen,",
         year: "1984",
         video: "https://www.youtube.com/embed/f4Mc-NYPHaQ?si=3SggS9XyTQxK9qmH",
+        imp: "The gayest song from Queen’s catalogue was surprisingly not written by Freddie Mercury, but rather bassist John Deacon. This anthem had the whole band dress in soap opera-inspired drag characters for its music video.",
     },
 
     {
@@ -28,6 +32,7 @@ const songs = [
         artist: "MUNA,",
         year: "2017",
         video: "https://www.youtube.com/embed/-t5gGm3NWU4?si=bgiwDSvWxLagyGsT",
+        imp: "Released shortly after the 2016 Orlando nightclub shooting, MUNA’s “I Know a Place” provides a virtual safe space of acceptance and solidarity, where queer folks can come and be received with understanding and love.",
     },
 
     {
@@ -35,6 +40,7 @@ const songs = [
         artist: "Village People,",
         year: "1978",
         video: "https://www.youtube.com/embed/9Ed_WWmFy9I?si=k6X_H62chFMmv1M5",
+        imp: "This disco classic was taken by many as an implicit ode to the gym’s reputation as a popular cruising location — it even comes from an album titled Cruisin’. “Y.M.C.A.” hit No. 2 on the Hot 100 in February 1979, but has since become a parody of itself. Thankfully, Grindr exists these days.",
     },
 
     {
@@ -50,6 +56,7 @@ const songs = [
         artist: "Mary Lambert,",
         year: "2013",
         video: "https://www.youtube.com/embed/NhqH-r7Xj0E?si=2QmsJ56mRmZL9S9c",
+        imp: "An outspoken lesbian and a Christian, Seattle singer-songwriter Mary Lambert sheds her shame and gets swept away by the ultimate dream girl in “She Keeps Me Warm.” Lambert drafted the song from her hook in “Same Love,” the 2012 hit single by Macklemore and Ryan Lewis. The trio would later make headlines after the 2014 Grammy Awards, where their landmark performance of the song would soundtrack a mass wedding between 33 gay and straight couples. (Not to mention it was officiated by Queen Latifah.) “I’m not crying on Sundays,” she sings impassionately – and it feels just like a prayer.",
     },
 
     {
@@ -57,6 +64,7 @@ const songs = [
         artist: "Tegan and Sara,",
         year: "2013",
         video: "https://www.youtube.com/embed/9e9NSMY8QiQ?si=MA9uukj_E8aP2P_l",
+        imp: "Canadian sister act Tegan and Sara capture the thrill behind fleeting moments of intimacy in “Closer,” a track from their platinum 2013 LP Heartthrob. The video depicts friends and lovers of all genders, cuddling in blanket forts and taking cosmetic trust falls by applying each others' makeup. It’s a breathtaking portrait of queer friendship, describing love that exists not just in the sexual or romantic sense, but as a broader spectrum of good feelings.", 
     },
 
     {
@@ -64,6 +72,7 @@ const songs = [
         artist: "Chapell Roan,",
         year: "2024",
         video: "https://youtu.be/VZ-oGLluGAc?si=3Jj_iagkUL7cXITm",
+        imp: "It's not easy when the person you love isn’t as accepting of their identity as you are, but on her breakout hit, Roan dances through the heartbreak. She also attempts to save a lot of time for others who are questioning their sexual orientations with lines like “You can kiss a hundred boys in bars … Make a new excuse, another stupid reason” and “You'd have to stop the world just to stop the feeling."
     },
 
     {
@@ -71,6 +80,7 @@ const songs = [
         artist: "Clairo,",
         year: "2019",
         video: "https://www.youtube.com/embed/L9l8zCOwEII?si=-vYzTurQy0tBWxt1",
+        imp: "On “Sofia,” Clairo touches on the painful — and all too relatable — internalized shame she and/or her partner feels about their same-sex romance. “Know that you and I shouldn’t feel like a crime,” she sings on the deceptively danceable track.",
     },
 
     {
@@ -78,6 +88,7 @@ const songs = [
         artist: "Billie Eilish,",
         year: "2024",
         video: "https://www.youtube.com/embed/MB3VkzPdgLA?si=U8TF31HjR7vhK2MU",
+        imp: "When Eilish dropped her third studio album Hit Me Hard and Soft in 2024, the immediate standout track was “Lunch” — a swaggering, flirtatious jam that finds the star explicitly singing about her attraction to women for the first time ever.",
     },
 
     {
@@ -85,6 +96,7 @@ const songs = [
         artist: "Madonna,",
         year: "1990",
         video: "https://www.youtube.com/embed/GuJQSAiODqI?si=BAipC3_fC8nSP_3U",
+        imp: "It's quite the task to choose just one track from Madonna's discography, but the safest bet is this song, inspired by queer vogueing subculture. The tune was originally intended as a B-side, but ended up topping the Hot 100 for three weeks in the spring of 1990.",
     },
 
     {
@@ -92,6 +104,7 @@ const songs = [
         artist: "Judy Garland,",
         year: "1939",
         video: "https://www.youtube.com/embed/EKiiSRzukAc?si=56tLpUy_imDCZp7p",
+        imp: "Dating back to at least World War II — when homosexual acts were illegal — the term “friend of Dorothy” was underground slang for a gay man. While investigating homosexuality in Chicago, the Naval Criminal Investigative Service discovered that gay men used this term to refer to themselves. They started a massive witch-hunt for the elusive “Dorothy” in hopes that she would reveal names of gay service members. This universal song of yearning from the immortal The Wizard of Oz won an Oscar for best original song.",
     },
 
     {
@@ -191,18 +204,21 @@ function randomArrayIndex(arr) {
 }
 
 
-document.addEventListener("click", loadRandom); 
+document.addEventListener("click", loadRandom);
 function loadRandom() {
     let result = randomArrayIndex(songs);
     console.log(result);
     let div = document.querySelector(".result");
 
     div.innerHTML = `
-        <iframe class="video">${result.video}
-        <h3 class="title">${result.title}
-          <h4 class="artist">${result.artist}
-          <span class="year">${result.year}</span>
-          
+       <iframe width="700" height="500" src="${result.video}"
+        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen> 
+    </iframe>
+        <h3 class="title">${result.title}</h3>
+        <h4 class="artist">${result.artist}</h4>
+        <h5 class="year">${result.year}</h5>
+        <div class="rainbow-box">${result.imp}> 
+           
 
     `;
 };
