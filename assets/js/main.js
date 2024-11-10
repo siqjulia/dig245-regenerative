68e108e9d68a505432799adc4ff9dd196749e887
siqjulia <jusiqueira@davidson.edu>
2024-11-10 16:16:34 -0500

    Added new logo png without bottom space

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,7 +204,7 @@ function loadRandom() {
     let div = document.querySelector(".result");
 
     div.innerHTML = `
-       <iframe width="600" height="400" src="${result.video}"
+       <iframe width="550" height="400" src="${result.video}"
         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen> 
     </iframe>
         <h3 class="title">${result.title}</h3>
